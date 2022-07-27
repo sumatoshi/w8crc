36100cad103bbf626deed922474b457d95eae391
Maxim Cege <msoyge@gmail.com>
2022-07-28 00:55:45 +0500

    Add writeFile from 1.1 to run tests on 1.0

--- tests/tfromfile_vs_lut.nim
+++ tests/tfromfile_vs_lut.nim
@@ -9,6 +9,19 @@ let
   mbh = 524288
   fname = "w8crc_fromfile_test.bin"
 
+proc writeFile(filename: string, content: openArray[byte]) =
+  ## Opens a file named `filename` for writing. Then writes the
+  ## `content` completely to the file and closes the file afterwards.
+  ## Raises an IO exception in case of an error.
+  var f: File = nil
+  if open(f, filename, fmWrite):
+    try:
+      f.writeBuffer(unsafeAddr content[0], content.len)
+    finally:
+      close(f)
+  else:
+    raise newException(IOError, "cannot open: " & filename)
+
 suite "crcfromfile(lookup)=crclut(lookup)":
   ## Check crcFromFile(LookupLut)
   setup:
